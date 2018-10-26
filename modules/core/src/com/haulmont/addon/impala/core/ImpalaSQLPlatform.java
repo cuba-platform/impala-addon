44cdd1a95f829e4dcf10a9eb9de1fd51ad46bc47
Andrey Subbotin <subbotin@haulmont.com>
2018-10-26 16:37:08 +0400

    CUBA Impala integration

---
+++ modules/core/src/com/haulmont/addon/impala/core/ImpalaSQLPlatform.java
@@ -0,0 +1,109 @@
+package com.haulmont.addon.impala.core;
+
+import org.eclipse.persistence.exceptions.ValidationException;
+import org.eclipse.persistence.internal.databaseaccess.DatabaseCall;
+import org.eclipse.persistence.internal.expressions.ExpressionSQLPrinter;
+import org.eclipse.persistence.internal.expressions.SQLSelectStatement;
+import org.eclipse.persistence.platform.database.DatabasePlatform;
+import org.eclipse.persistence.queries.ValueReadQuery;
+
+import java.io.Writer;
+
+public class ImpalaSQLPlatform extends DatabasePlatform {
+
+    @Override
+    protected void initializePlatformOperators() {
+        super.initializePlatformOperators();
+    }
+
+    @Override
+    public boolean supportsUniqueColumns() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsIdentity() {
+        return false;
+    }
+
+    @Override
+    public ValueReadQuery buildSelectQueryForIdentity() {
+        throw new UnsupportedOperationException("Impala doesn't support identity");
+    }
+
+    @Override
+    public void printFieldIdentityClause(Writer writer) throws ValidationException {
+        throw new UnsupportedOperationException("Impala doesn't support identity");
+    }
+
+    /**
+     * JDBC escape syntax for outer joins is not supported (not required).
+     */
+    @Override
+    public boolean shouldUseJDBCOuterJoinSyntax() {
+        return false;
+    }
+
+
+    @Override
+    public boolean supportsSequenceObjects() {
+        return false;
+    }
+
+    @Override
+    public boolean isAlterSequenceObjectSupported() {
+        return false;
+    }
+
+    @Override
+    public ValueReadQuery buildSelectQueryForSequenceObject(String seqName, Integer size) {
+        throw new UnsupportedOperationException("Impala doesn't support sequences");
+    }
+
+
+    @Override
+    public int computeMaxRowsForSQL(int firstResultIndex, int maxResults) {
+        return maxResults - ((firstResultIndex >= 0) ? firstResultIndex : 0);
+    }
+
+    /**
+     * Print the pagination SQL using Impala syntax "SELECT ... FROM ... LIMIT {@literal <max>} OFFSET {@literal <max>}".
+     */
+    @Override
+    public void printSQLSelectStatement(DatabaseCall call, ExpressionSQLPrinter printer, SQLSelectStatement statement) {
+        int max = 0;
+        int firstRow = 0;
+
+        if (statement.getQuery() != null) {
+            max = statement.getQuery().getMaxRows();
+            firstRow = statement.getQuery().getFirstResult();
+        }
+
+        statement.setUseUniqueFieldAliases(true);
+        call.setFields(statement.printSQL(printer));
+
+        if (max > 0) {
+            printer.printString(" LIMIT ");
+            printer.printParameter(DatabaseCall.MAXROW_FIELD);
+        }
+
+        if (firstRow > 0) {
+            printer.printString(" OFFSET ");
+            printer.printParameter(DatabaseCall.FIRSTRESULT_FIELD);
+        }
+
+        call.setIgnoreFirstRowSetting(true);
+        call.setIgnoreMaxResultsSetting(true);
+    }
+
+    @Override
+    public ValueReadQuery getTimestampQuery() {
+        //Impala doesn't support timestamp query
+        return null;
+    }
+
+    @Override
+    public Writer buildSequenceObjectCreationWriter(Writer writer, String fullSeqName, int increment, int start) {
+        throw new UnsupportedOperationException("Impala doesn't support sequences");
+    }
+}
---
+++ modules/core/src/com/haulmont/cuba/core/sys/persistence/ImpalaDbmsFeatures.java
@@ -0,0 +1,71 @@
+package com.haulmont.cuba.core.sys.persistence;
+
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImpalaDbmsFeatures implements DbmsFeatures {
+    @Override
+    public Map<String, String> getJpaParameters() {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("eclipselink.target-database", "com.haulmont.addon.impala.core.ImpalaSQLPlatform");
+        return params;
+    }
+
+    @Override
+    public String getIdColumn() {
+        return "id";
+    }
+
+    @Override
+    public String getDeleteTsColumn() {
+        return "delete_ts";
+    }
+
+    @Override
+    public String getTimeStampType() {
+        return "timestamp";
+    }
+
+    @Nullable
+    @Override
+    public String getUuidTypeClassName() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getTransactionTimeoutStatement() {
+        return null;
+    }
+
+    @Override
+    public String getUniqueConstraintViolationPattern() {
+        return "ERROR: duplicate key value violates unique constraint \"(.+)\"";
+    }
+
+    @Override
+    public boolean isNullsLastSorting() {
+        return true;
+    }
+
+    @Override
+    public boolean isSchemaByUser() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsLobSortingAndFiltering() {
+        return false;
+    }
+
+    @Override
+    public boolean isEmulatesEqualsAsLike() {
+        return true;
+    }
+
+    @Override
+    public boolean useOrderByForPaging() {
+        return true;
+    }
+}
